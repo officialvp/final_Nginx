4d3a39308c3329802bc1e06602860be3667b0f7e
Vivek Patel <vp75061@gmail.com>
2023-01-28 04:46:13 +0000

    First Commit

---
+++ main.tf
@@ -0,0 +1,79 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAUGEVBVXRMCNYFPSI"
+  secret_key = "08gZCV7FPYeyF36Awv9gwHWfuw3DTAKSXDTYL+q+"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.example.id
+  vpc      = true
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+}
+
+resource "aws_route_table_association" "example" {
+  subnet_id      = aws_subnet.example.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_security_group" "example" {
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0aedf6b1cb669b4c7"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.example.id
+  vpc_security_group_ids = [aws_security_group.example.id]
+  key_name = "ansible"
+}
+
